b425d83eba95e0643dea97b031637bd37e895e44
tharlysdias <tharlys.souza@outlook.com>
2021-05-15 20:51:09 -0300

    Calculo de matrizes

--- Exercicios/PSC/src/matrizes/Matriz_002.java
+++ Exercicios/PSC/src/matrizes/Matriz_002.java
@@ -4,13 +4,15 @@ import java.util.Scanner;
 
 public class Matriz_002 {
 	
+	static Scanner teclado;
+
 	/**
 	 * Função para ler matriz A e B
 	 * 
 	 */
 	public static int[][] lerMatriz() {
 		int matrizTemp[][] = new int[3][2];
-		
+
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 2; j++) {
 				System.out.print("Digite o número da posição " + (j + 1) + "º que compõe a matrizTemp: ");
@@ -19,56 +21,61 @@ public class Matriz_002 {
 		}
 		return matrizTemp;
 	}
-	
+
 	public static int numero() {
-		Scanner teclado = new Scanner(System.in);
+		teclado = new Scanner(System.in);
 		int numeroTemp = teclado.nextInt();
 		return numeroTemp;
 	}
 
-	public static void main(String[] args) {
+	public static void matrizes() {
+		int matrizA[][] = { { 5, 4 }, { 0, 2 }, { 1, -1 } };
+		int matrizB[][] = { { 0, -2 }, { 5, -3 }, { -1, 0 } };
+		int matrizC[][] = new int[3][2];
 
-//		int matrizA[][] = { { 5, 4 }, { 0, 2 }, { 1, -1 } };
-//		int matrizB[][] = { { 0, -2 }, { 5, -3 }, { -1, 0 } };
-//		int matrizC[][] = new int[3][2];
-
-//		System.out.println("Todos os preços dos produtos das lojas: ");
-//		for (int i = 0; i < matrizB.length; i++) {
-//			for (int j = 0; j < matrizA.length; j++) {
-//				System.out.println("Digite o preço do produto " + matrizA[j] + " da loja " + matrizB[i]);
-//				matrizC[i][j] = teclado.nextDouble();
-//			}
-//		}
-//
-//		for (int i = 0; i < 3; i++) {
-//			for (int j = 0; j < 2; j++) {
-//				matrizC[i][j] = matrizA[i][j] + matrizB[i][j];
-//			}
-//		}
-//
-//		for (int i = 0; i < 3; i++) {
-//			System.out.println("\n");
-//			for (int j = 0; j < 2; j++) {
-//				System.out.print("\t" + matrizC[i][j]);
-//			}
-//		}
-		
-		int matriz1[][] = lerMatriz();
-		int matriz2[][] = lerMatriz();
-		int matriz3[][] = new int[3][2];
-		
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 2; j++) {
-				matriz3[i][j] = matriz1[i][j] + matriz2[i][j];
+				matrizC[i][j] = matrizA[i][j] + matrizB[i][j];
 			}
 		}
 
 		for (int i = 0; i < 3; i++) {
 			System.out.println("\n");
 			for (int j = 0; j < 2; j++) {
-				System.out.print("\t" + matriz3[i][j]);
+				System.out.print("\t" + matrizC[i][j]);
 			}
 		}
+	}
+
+	public static void main(String[] args) {
+
+		int opcao;
+		System.out.println("Boas vindas, você está no sistema de matrizes!");
+		System.out.print("Digite 0 para matrizes preenchidas e 1 para preencher as matrizes: ");
+		opcao = numero();
+		if (opcao == 0) {
+			matrizes();
+		} else if (opcao == 1) {
+			int matriz1[][] = lerMatriz();
+			int matriz2[][] = lerMatriz();
+			int matriz3[][] = new int[3][2];
+
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < 2; j++) {
+					matriz3[i][j] = matriz1[i][j] + matriz2[i][j];
+				}
+			}
+
+			for (int i = 0; i < 3; i++) {
+				System.out.println("\n");
+				for (int j = 0; j < 2; j++) {
+					System.out.print("\t" + matriz3[i][j]);
+				}
+			}
+		} else {
+			System.out.println("Opção errada seu noob!!!");
+		}
+		teclado.close();
 	}
 
 }
