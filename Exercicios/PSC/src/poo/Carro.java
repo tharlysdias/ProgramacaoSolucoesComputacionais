5b2985ff75f5ab151bec37f24d50bbdbe8325ad1
tharlysdias <tharlys.souza@outlook.com>
2021-05-21 20:14:56 -0300

    POO exemplo

---
+++ Exercicios/PSC/src/poo/Carro.java
@@ -0,0 +1,14 @@
+package poo;
+
+import java.util.Date;
+
+public class Carro {
+
+	String marcaVeiculo;
+	String modeloVeiculo;
+	Date dataFabricacaoVeiculo;
+	double pesoVeiculo;
+	int quantidadeMarchaVeiculo;
+	int quantidadePortaVeiculo;
+
+}
---
+++ Exercicios/PSC/src/poo/Fabricante.java
@@ -0,0 +1,12 @@
+package poo;
+
+public class Fabricante {
+
+	public static void main(String[] args) {
+
+		Carro carro1 = new Carro();
+		Carro carro2 = new Carro();
+		
+	}
+
+}
