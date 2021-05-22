3590a934ca418df95c06b73fd1c8d20f2d88ecfe
tharlysdias <tharlys.souza@outlook.com>
2021-05-21 21:53:21 -0300

    POO continuação 21:52 21/05

--- Exercicios/PSC/src/poo/Carro.java
+++ Exercicios/PSC/src/poo/Carro.java
@@ -4,11 +4,75 @@ import java.util.Date;
 
 public class Carro {
 
-	String marcaVeiculo;
-	String modeloVeiculo;
-	Date dataFabricacaoVeiculo;
-	double pesoVeiculo;
-	int quantidadeMarchaVeiculo;
-	int quantidadePortaVeiculo;
+	private String marcaVeiculo;
+	private String modeloVeiculo;
+	private Date dataFabricacaoVeiculo;
+	private double pesoVeiculo;
+	private int quantidadeMarchaVeiculo;
+	private int quantidadePortaVeiculo;
+
+	public void ligar() {
+		System.out.println("Ligar o carro!  ->  " + this.toString());
+	}
+
+	public void desligar() {
+		System.out.println("Desligar o carro");
+	}
+
+	public void acelerar() {
+		System.out.println("Acelerar o carro");
+	}
+
+	public String freiar() {
+		return "Freiar o carro";
+	}
+
+	public String getMarcaVeiculo() {
+		return marcaVeiculo;
+	}
+
+	public void setMarcaVeiculo(String marcaVeiculo) {
+		this.marcaVeiculo = marcaVeiculo;
+	}
+
+	public String getModeloVeiculo() {
+		return modeloVeiculo;
+	}
+
+	public void setModeloVeiculo(String modeloVeiculo) {
+		this.modeloVeiculo = modeloVeiculo;
+	}
+
+	public Date getDataFabricacaoVeiculo() {
+		return dataFabricacaoVeiculo;
+	}
+
+	public void setDataFabricacaoVeiculo(Date dataFabricacaoVeiculo) {
+		this.dataFabricacaoVeiculo = dataFabricacaoVeiculo;
+	}
+
+	public double getPesoVeiculo() {
+		return pesoVeiculo;
+	}
+
+	public void setPesoVeiculo(double pesoVeiculo) {
+		this.pesoVeiculo = pesoVeiculo;
+	}
+
+	public int getQuantidadeMarchaVeiculo() {
+		return quantidadeMarchaVeiculo;
+	}
+
+	public void setQuantidadeMarchaVeiculo(int quantidadeMarchaVeiculo) {
+		this.quantidadeMarchaVeiculo = quantidadeMarchaVeiculo;
+	}
+
+	public int getQuantidadePortaVeiculo() {
+		return quantidadePortaVeiculo;
+	}
+
+	public void setQuantidadePortaVeiculo(int quantidadePortaVeiculo) {
+		this.quantidadePortaVeiculo = quantidadePortaVeiculo;
+	}
 
 }
--- Exercicios/PSC/src/poo/Fabricante.java
+++ Exercicios/PSC/src/poo/Fabricante.java
@@ -6,7 +6,21 @@ public class Fabricante {
 
 		Carro carro1 = new Carro();
 		Carro carro2 = new Carro();
+		Carro carro3 = carro2;
 		
+		carro1.setMarcaVeiculo("Fiat");
+
+		carro1.ligar();
+		carro1.acelerar();
+		System.out.println(carro1.freiar());
+		carro1.desligar();
+		
+		carro2.ligar();
+		carro2.acelerar();
+		System.out.println(carro2.freiar());
+		carro2.desligar();
+		
+		carro3.ligar();
 	}
 
 }
